e930e3b1e622e3a6ac3d5a756ccf4996ebf0f5d1
Steve Calla <callasteven@gmail.com>
2022-07-06 22:53:34 -0600

    Create styling for question section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,14 @@ answerContainer.addEventListener('click', targetElement);
 //section:functions and event handlers go here 👇
 function targetElement(event) {
   console.log(event.target.innerText);
+  let selectedAnswer = event.target.innerText;
+  if (selectedAnswer === 'Alerts') {
+    console.log('correct');
+    //log correct below answer list
+  } else {
+    console.log('not correct');
+    //reduce score by 10 points
+    //reduce remaining time by 10 seconds
+    //log wrong! below answer list
+  }
 }
