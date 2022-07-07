9ed0ca05486302882794e989dd41ef914f4f4c20
Steve Calla <callasteven@gmail.com>
2022-07-07 15:35:12 -0600

    Added more questions:

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,28 +5,51 @@ const questionList = [
     correctAnswer: 'Alerts',
   }, 
   {
-    question: 'TBD #2',
-    answerList: ['a', 'b', 'c', 'd'],
-    correctAnswer: 'a',
+    question: 'The condition in an if / else statement is enclosed with _______________.',
+    answerList: ['Quotes', 'Curly brackets', 'Parenthesis', 'Square Brackets'],
+    correctAnswer: 'Parenthesis',
   }, 
-  // {
-  //   question: 'TBD #3',
-  //   answerList: ['a', 'b', 'c', 'd'],
-  //   correctAnswer: 'a',
-  // },
-  // {
-  //   question: 'TBD #4',
-  //   answerList: ['a', 'b', 'c', 'd'],
-  //   correctAnswer: 'a',
-  // }, 
-  // {
-  //   question: 'TBD #5',
-  //   answerList: ['a', 'b', 'c', 'd'],
-  //   correctAnswer: 'a',
-  // }, 
-  // {
-  //   question: 'TBD #6',
-  //   answerList: ['a', 'b', 'c', 'd'],
-  //   correctAnswer: 'a',
-  // },
+  {
+    question: 'Arrays in Javascript can be used to store _______________.',
+    answerList: ['Numbers and Strings', 'Other Arrays', 'Booleans', 'All of the above'],
+    correctAnswer: 'All of the above',
+  },
+  {
+    question: 'String values must be enclosed with _______________ when being assigned to variables.',
+    answerList: ['Commas', 'Curly brackets', 'Quotes', 'Parenthesis'],
+    correctAnswer: 'Quotes',
+  }, 
+  {
+    question: 'A very useful tool used during development and debugging for printing the content to the debugger is:',
+    answerList: ['Javascript', 'Termina/Bash', 'For Loops', 'console.log'],
+    correctAnswer: 'console.log',
+  }, 
+  {
+    question: 'Javascript is an _______ language?',
+    answerList: ['Object-Oriented', 'Objected-Based', 'Procedural', 'None of the above'],
+    correctAnswer: 'Object-Oriented',
+  },
+  {
+    question: 'Which of the following keywords is used to define a variable in Javascript?',
+    answerList: ['var', 'let', 'const, 'All of the above'],
+    correctAnswer: 'All of the above',
+  },
+  {
+    question: 'Which of the following methods is used to access HTML elements using Javascript??',
+    answerList: ['getElementByid()', 'getElementbyClassName', 'Both A and B', 'None of the above'],
+    correctAnswer: 'Object-Oriented',
+  },
+  {
+    question: 'Upon encountering empty statements, what does the Javascript Interpreter do?',
+    answerList: ['Throws an error', 'Ignores the statement', 'Gives a warning', 'None of the above'],
+    correctAnswer: 'Object-Oriented',
+  },
+  {
+    question: 'What is the use of the <noscript> tag in Javascript?',
+    answerList: ['The content is displayed by non-JS-based browswers', 'Clears all the cookies and cache', 'Both A and B', 'None of the above'],
+    correctAnswer: 'The content is displayed by non-JS-based browswers',
+  },
 ]
+
+// https://www.interviewbit.com/javascript-mcq/
+
