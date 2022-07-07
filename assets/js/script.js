416e1dd73a36c36fad0ed6b0be33e8e202c982e1
Steve Calla <callasteven@gmail.com>
2022-07-06 22:40:46 -0600

    Add order list id and create target variables and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 //section:query selector variables go here 👇
+let answerContainer = document.getElementById('answer-container');
+console.log(answerContainer)
 
 //section:global variables go here 👇
 
 //section:event listeners go here 👇
+answerContainer.addEventListener('click', targetElement);
 
 //section:functions and event handlers go here 👇
+function targetElement(event) {
+  console.log(event.target.innerText);
+}
