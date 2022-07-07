5fc26ce98a712a36b5f79d362967c5d1049717dd
Steve Calla <callasteven@gmail.com>
2022-07-07 00:35:36 -0600

    Add data js object. Adjust script js to use data js object . Adjust script js to inject question into html.

---
+++ assets/js/data.js
@@ -0,0 +1,15 @@
+const questionList = [
+  {
+    question: 'Commonly used data types DO NOT include:',
+    answerList: ['String', 'Boolean', 'Alerts', 'Numbers'],
+    correctAnswer: 'Alerts',
+  }, {
+    question: 'TBD #2',
+    answerList: ['a', 'b', 'c', 'd'],
+    correctAnswer: 'a',
+  }, {
+    question: 'TBD #3',
+    answerList: ['a', 'b', 'c', 'd'],
+    correctAnswer: 'a',
+  },
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //section:query selector variables go here 👇
 let startGameButton = document.getElementById('start-button');
-let answerContainer = document.getElementById('answer-container');
+// let answerContainer = document.getElementById('answer-container');
 let startPage = document.getElementById('start-page');
 let questionPage = document.getElementById('question-page');
 let answerStatus = document.getElementById('answer-status');
@@ -11,32 +11,47 @@ let saveButton = document.getElementById('save-button');
 
 //section:event listeners go here 👇
 startGameButton.addEventListener('click', displayQuestion);
-answerContainer.addEventListener('click', targetElement);
+// answerContainer.addEventListener('click', targetElement);
+questionPage.addEventListener('click', targetElement);
 saveButton.addEventListener('click', saveInitialsAndScore)
 
 //section:functions and event handlers go here 👇
 function displayQuestion() {
-  // questionPage.classList.remove('hide'); //todo:add back to play game; also add hide class to question page
-  // startPage.classList.add('hide'); //todo:add back to play game
+  questionPage.classList.remove('hide'); //todo:add back to play game; also add hide class to question page
+  startPage.classList.add('hide'); //todo:add back to play game
+  questionPage.innerHTML = `
+    <h1>${questionList[0].question}</h1>
+    <ol id="answer-container">
+      <li>${questionList[0].answerList[0]}</li>
+      <li>${questionList[0].answerList[1]}</li>
+      <li>${questionList[0].answerList[2]}</li>
+      <li>${questionList[0].answerList[3]}</li>
+    </ol>
+  `
 }
 
 function targetElement(event) {
   console.log(event.target.innerText);
   let selectedAnswer = event.target.innerText;
 
-  answerContainer.style.borderBottom = "3px solid grey";
-  answerContainer.removeEventListener('click', targetElement);
+  questionPage.removeEventListener('click', targetElement);
+  
+  // Creates an H3 element for the answer status
+  var tag = document.createElement('h3');
+  tag.setAttribute('id', 'answer-status');
+  tag.style.borderTop = "3px solid grey";
+   // Appends tag as child of question page
+   questionPage.appendChild(tag);
 
-  if (selectedAnswer === 'Alerts') {
+
+  if (selectedAnswer === questionList[0].correctAnswer) {
     console.log('correct');
-    //log correct below answer list
-    answerStatus.innerText = "Correct!!"
+    tag.textContent = "Correct!"; // Adds text content to created tag
   } else {
     console.log('not correct');
+    tag.textContent = "Wrong!"; // Adds text content to created tag
     //reduce score by 10 points
     //reduce remaining time by 10 seconds
-    //log wrong! below answer list\
-    answerStatus.innerText = "Wrong!"
   }
 }
 
